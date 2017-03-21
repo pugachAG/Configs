bd0e6f289163b48afc916499de23f6dc9cacfca8
pugachag <pugachag@gmail.com>
2017-03-21 22:11:59 +0100

    Remove mac-related trash

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM phusion/baseimage
-
-ADD . /tmp/config/
-RUN sh /tmp/config/configure_container.sh
-
-ENV TERM xterm
-
--- install_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git submodule update --init --recursive
-./lenaps configure
-cp ./lenaps /usr/local/bin/
-cp ./mac_clip/clip_serv.py /usr/local/bin/
-
